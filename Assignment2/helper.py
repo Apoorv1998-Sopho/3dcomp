77df869ea1ad03dd8ae7a36a5d84f64506e46ffb
Apoorv <apoorvagni@hotmail.com>
2018-09-26 11:26:52 +0530

    Started assignment2 Question a done
    Working of questionb

--- Assignment2/helper.py
+++ Assignment2/helper.py
@@ -6,13 +6,14 @@ Assignment 2: Sift
 '''
 
 import numpy as np
-import ../Assignment1/helper as hp
+import cv2 as cv
+from old_helper import *
 
 '''
 here dim is the dimention of gaussian filter
 '''
 class SIFT(object):
-    def __init__ (self, sigma, k=2**.5, dim=5\
+    def __init__ (self, sigma, k=2**.5, dim=5,\
                   numofoctaves=4, numofscales=5):
         self.sigma = sigma
         self.k = k
@@ -20,33 +21,32 @@ class SIFT(object):
         self.numofscales = numofscales
         self.dim = dim
 
+    def generateScales(self, img):
+        scales=[]
+        for i in range(self.numofscales):
+            #find the related sigam
+            newsigma = self.sigma*(self.k**i)
+            scales.append(self.blur(img, newsigma))
+        return scales
+
+    def generateOctaves(self, img):
+        octaves=[]
+        for i in range(self.numofoctaves):
+            newsigma = (self.k ** (2*i - 1)) * self.sigma
+            scale = 1./(2**i)
+            newbaseimage= cv.resize(img, None, \
+                                fx = scale, \
+                                fy = scale, \
+                                interpolation = cv.INTER_CUBIC)
+            octaves.append(generateScales(img,newsigma))
+        return octaves
 
 
-def generateScales(img):
-    scales=[]
-    for i in range(self.numofscales):
-        #find the related sigam
-        newsigma = self.sigma*(self.k**i)
-        scales.append(self.blur(img, newsigma))
-    return scales
-
-def generateOctaves(img):
-    octaves=[]
-    for i in range(self.numofoctaves):
-        newsigma = (self.k ** (2*i - 1)) * self.sigma
-        newbaseimage= cv.resize(img, None, \
-                            fx = scale, \
-                            fy = scale, \
-                            interpolation = cv.INTER_CUBIC)
-        octaves.append(generateScales(img,newsigma))
-
-        
-
-'''
-Runs a normalized gaussian filter over the image
-and returns the result with same datatype
-'''
-def blur(img, newsigma)
-    gauss = hp.gaussian_nor(newsigma)
-    return hp.covolv(img, gauss)
+    '''
+    Runs a normalized gaussian filter over the image
+    and returns the result with uint8 datatype
+    '''
+    def blur(self, img, newsigma):
+        gauss = hp.gaussian_nor(self.dim, newsigma)
+        return hp.covolv(img, gauss, np.uint8)
 
---
+++ Assignment2/old_helper.py
@@ -0,0 +1,171 @@
+import numpy as np
+import cv2 as cv
+import matplotlib.pyplot as plt
+import math
+
+
+# padding function
+def padd(target, padding):
+    #image size
+    n, m = target.shape # n x m
+    newTarget = np.zeros((target.shape[0] + 2*padding,\
+                          target.shape[1] + 2*padding))
+    newTarget[padding:n + padding, padding:m + padding] = target
+    return newTarget
+
+
+# covolution function
+def covolv(target, filtr, typ=None):
+    #assuming the filter is odd x odd
+    padding = int(int(filtr.shape[0])/2)
+    filterSize = filtr.shape[0]
+    n, m = target.shape # n x m
+
+    # gave extra padding to original image and made a big matrix to return
+    padTarget = padd(target, padding)
+    NewTarget = np.zeros((n + padding, m + padding))
+    
+    #move filter over all points in matrix
+    for trxx in range(padding, n + padding):
+        for tryy in range(padding, m + padding):
+            s = 0
+            
+            #find the dot product of filter + submatrix
+            for fx in range(filterSize):
+                for fy in range(filterSize):
+                    srx = trxx + fx - padding
+                    sry = tryy + fy - padding
+                    s += filtr[fx, fy] * padTarget[srx, sry]
+            NewTarget[trxx,tryy] = s
+
+    # returning trimmed big matrix and typecated to type provided
+    if typ == None:
+        typ = filtr.dtype
+    return NewTarget[padding:n + padding, padding:m + padding].astype(typ)
+
+
+# gaussian filter (normalized) generator function
+def gaussian_nor(dim, std):
+    filtr = gaussian(dim, std)
+    return normalize(filtr) 
+
+# gaussian filter, not normalized
+def gaussian(dim, std):
+    filtr = np.zeros((dim, dim))
+    padding = int(dim/2)
+
+    for fx in range(dim):
+        for fy in range(dim):
+            x = fx - padding
+            y = fy - padding
+            # using the gaussian equation to find the value at pixel x,y
+            g = 1./(2*math.pi*std**2) * math.exp(-(x**2 + y**2)/(2*std**2))
+            filtr[fx, fy] = g
+    
+    return filtr
+
+# normalizing the filter
+def normalize(filtr):
+    dim, dim = filtr.shape
+    r_filtr = np.zeros((dim, dim))
+    normalizer = 0
+    for i in range(dim):
+        for k in range(dim):
+            '''
+            Assumption:
+               As we saw that sobel filter
+               sum is 0 but we don't divide the
+               filter by 0, instead we divide by 
+               sum of absolute values of each 
+               element in the filter, similiarly I 
+               have taken summation of absolute 
+               values for normalizing.
+            '''
+            normalizer += abs(filtr[i,k])
+            
+    # dividing the filter elements with the sum of all the elements.
+    for i in range(dim):
+        for k in range(dim):
+            r_filtr[i,k] = filtr[i,k]/normalizer
+    
+    return r_filtr    
+
+
+# printing in scientific notations
+def prettyPrint(ary):
+    n,m = ary.shape
+    for i in range(n):
+        for j in range(m - 1):
+            print('{:.3e}'.format(ary[i,j]) + ' | ', end='')
+        print ('{:.3e}'.format(ary[i,m-1]))
+
+def readImg_Grey_Resize(file, scale=1):
+    I1 = cv.imread('imgs/3_1.jpg')
+    height, width, channels = I1.shape
+    
+    # show raw img
+    cv.namedWindow('raw', cv.WINDOW_NORMAL)
+    cv.imshow('raw',I1)
+    cv.waitKey(0)
+    cv.destroyAllWindows()
+    # convert to grey
+    I1_gray = cv.cvtColor(I1,cv.COLOR_BGR2GRAY);
+    
+    # show greyed image
+    cv.namedWindow('grey', cv.WINDOW_NORMAL)
+    cv.imshow('grey',I1_gray)
+    cv.waitKey(0)
+    cv.destroyAllWindows()
+    
+    # resize img
+    if (scale != 1):
+        I1_resized = cv.resize(I1_gray,None, fx = scale, fy = scale, \
+                         interpolation = cv.INTER_CUBIC)
+        cv.namedWindow('resized', cv.WINDOW_NORMAL)
+        cv.imshow('resized',I1_resized)
+        cv.waitKey(0)
+        cv.destroyAllWindows()
+        return I1_resized
+    
+    else:
+        return I1_gray
+
+''' 
+brief:
+detecting Zero Crossings, in a given array.
+
+@param arry: input matrix
+@param method: among the possible methods
+  choose a specific method to find zero
+  crossings
+  
+  (0) - see for sign change (+ to -) in 4
+   neigbours.
+  
+  (1) - make any pixel within epsilon 255 (edge)
+  
+@param epsilon corresponds to method = 1
+'''
+def detectZeroCrossings(arry, method = 0, epsilon = 1):
+    n, m = arry.shape
+    r = np.zeros((n,m), dtype=np.uint8)
+    
+    if (method == 0):
+        for i in range(n):
+            for k in range(m):
+                try:
+                    s = min(arry[i,k-1], arry[i,k+1], \
+                            arry[i-1,k], arry[i+1,k])
+                    if(arry[i,k]>=0 and s < 0):
+                        r[i,k] = 255
+                except IndexError:
+                    r[i,k] = 255
+    else: #(method == 1)
+        for i in range(n):
+            for k in range(m):
+                if (abs(arry[i, k]) <= epsilon):
+                    r[i,k] = 255
+                else:
+                    r[i,k] = 0
+    
+    return r
