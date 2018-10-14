753ef7965c64fb257d7953e3210511d409ffc464
Apoorv <apoorvagni@hotmail.com>
2018-10-14 15:56:28 +0530

    Making homography

--- A3/main1.py
+++ A3/main1.py
@@ -4,6 +4,7 @@ Author: Apoorv Agnihotri
 Code samples from:
 https://kushalvyas.github.io/stitching.html
 '''
+
 # images is a list of numpy arrays, containing images
 def keyPoints(images, imagesNames): # add an option to send a list of strings, where keypoints return
     # for every image find keypoint discriptors
@@ -22,7 +23,7 @@ def keyPoints(images, imagesNames): # add an option to send a list of strings, w
     # compare each image with every other
     return (imageKeyPoints, imageDescriptors)
 
-def keyPointMatching(imageDescriptors, imgA, imgB):
+def keyPointMatching(imageKeyPoints, imageDescriptors, imgA, imgB):
     FLANN_INDEX_KDTREE = 0
     index_params = dict(algorithm=FLANN_INDEX_KDTREE, trees=5)
     search_params = dict(checks=50)   # or pass empty dictionary
@@ -31,17 +32,18 @@ def keyPointMatching(imageDescriptors, imgA, imgB):
     # print('1 ', len(imageDescriptors[0]))
     # print('2 ', len(imageDescriptors[1]))
     # print (type(imageDescriptors[imgA]))
-    matches = flann.knnMatch(imageDescriptors[imgB],
-                             imageDescriptors[imgA], k=2)
+    matches = flann.knnMatch(imageDescriptors[imgA],
+                             imageDescriptors[imgB], k=2)
                              # matches 2 nearest neigbours
 
     #using lows ratio test
-    good = []
+    good = [[],[]]
     for i, (m, n) in enumerate(matches):
-        if m.distance < 0.7 * n.distance: # if closest match is ratio 
+        if m.distance < 0.8 * n.distance: # if closest match is ratio 
                                           # closer than the second closest one,
                                           # then the match is good
-            good.append((m.trainIdx, m.queryIdx))
+            good[0].append(imageKeyPoints[imgA][m.queryIdx].pt)
+            good[1].append(imageKeyPoints[imgB][m.trainIdx].pt)
 
     #print(type(matches))
     matchesMask = [[0,0] for i in range(len(matches))]
@@ -56,7 +58,113 @@ def keyPointMatching(imageDescriptors, imgA, imgB):
     # print('length of all matches ', len(matches))
     # print('length good matches ', len(good))
     cv.waitKey()
+    # print (len(good[0])) # length of good keyPointS
     return good
+
+'''
+n is the number of times to repeat ransac
+t is the number of pixels tolerance allowed
+T is the ratio of the for which we terminate early.
+'''
+def findHomoRanSac(n, r, list_kp, t, T):
+    list_kp1 = list_kp[0]
+    list_kp2 = list_kp[1]
+    length = len(list_kp)
+    print(len(list_kp[0])) # no of good matches
+    print(list_kp1[0]) # random point
+
+    for i in range(n):
+        list_kp1r = []
+        list_kp2r = []
+        Sis = []
+        Sisno = []
+        # selecting ramdomly r points
+        for i in range(r):
+            key = np.random.choice(length)
+            list_kp1r.append(list_kp1[key])
+            list_kp2r.append(list_kp2[key])
+
+        # find the homo, inlier set
+        P = make_P(list_kp1r, list_kp2r)
+        H, Si = findH_Si(P, list_kp, t)
+        print ("length of the Si, line no 85:",len(Si))
+        Sis.append(Si)
+        Sisno.append(len(Si))
+
+        # if majority return with new H
+        if len(Si) >= T:
+            P = make_P(Si[0], Si[1])
+            H, Si = findH_Si(P, list_kp, t)
+            return H
+
+    Sisnoi = np.argmax(np.array(Sisno))[0] # taking the first index 
+                                           # with global max cardinality
+    Si = Sis[Sisnoi]
+    P = make_P(Si[0], Si[1])
+    H, Si = findH_Si(P, list_kp, t)
+    return H
+
+
+def findH_Si(P, list_kp, t):
+    # do svd on P get perlimns H
+    u, s, vh = np.linalg.svd(P, full_matrices=True)
+    H = vh.T[:, -1] # taking the last singular vector
+    H = np.reshape(H, (3,3))
+    Si = [[],[]]
+
+    # multiply all the matches and find if within tol
+    initialPts = list_kp[0]
+    finalPts = list_kp[1]
+    for i in range(len(initialPts)):
+        inPt = initialPts[i]
+        fPt = finalPts[i]
+        vc = np.array([[fPt[0]],[fPt[1]], [1]])
+        vi = np.array([[inPt[0]],[inPt[1]], [1]])
+        vf = np.matmul(H, vc)
+        # print ('shape of vf', vf.shape)
+        # making the last coordinate 1
+        vf /= vf[2,0]
+
+        # check if within some tolerance
+        if np.linalg.norm(vf - vc) <= t:
+            Si[0].append(inPt)
+            Si[1].append(fPt)
+    return (H, Si)
+
+'''
+I assume that i recieve 2 lists, in which i have k points
+'''
+def make_P(list_kp1, list_kp2):
+    k = len(list_kp1)
+    # print ('list_kp1', list_kp1)
+
+    # making P matrix
+    P = np.zeros((2*k, 9))
+    for i in range(0,2*k,2):
+        x = list_kp1[int(i/2)][0]
+        x_ = list_kp2[int(i/2)][0]
+        y = list_kp1[int(i/2)][1]
+        y_ = list_kp2[int(i/2)][1]
+
+        P[i+0,0] = -x
+        P[i+0,1] = -y
+        P[i+0,2] = -1
+        P[i+0,6] = x*x_
+        P[i+0,7] = y*x_
+        P[i+0,8] = x_
+        P[i+1,3] = -x
+        P[i+1,4] = -y
+        P[i+1,5] = -1
+        P[i+1,6] = x*y_
+        P[i+1,7] = y*y_
+        P[i+1,8] = y_
+    return P
+
+def stitch(imgA, imgB, H, s, ratio=0.75, reporjThrest=4.0):
+    result = cv.warpPerspective(imgA, H, (imgA.shape[1] + imgB.shape[1], imgA.shape[0]))
+    result[0:imgB.shape[0], 0:imgB.shape[1]] = imgB
+    return result
+
     # if len(good) > 4:
     #     pointsCurrent = imageKeyPoints[1]
     #     pointsPrevious = imageKeyPoints[0]
@@ -71,31 +179,21 @@ def keyPointMatching(imageDescriptors, imgA, imgB):
     #     H, s = cv.findHomography(matchedPointsCurrent, matchedPointsPrev, cv.RANSAC, 4)
     # return (H, s)
 
-
-def stitch(imgA, imgB, H, s, ratio=0.75, reporjThrest=4.0):
-    result = cv.warpPerspective(imgA, H, (imgA.shape[1] + imgB.shape[1], imgA.shape[0]))
-    result[0:imgB.shape[0], 0:imgB.shape[1]] = imgB
-    return result
-
-
-
 ##########################################################
 import cv2 as cv
 import numpy as np
 import sys
 
-
 ##########################################################
 #Reading files
 ##########################################################
 path = './Images_Asgnmt3_1/I1/'
-imagesNames = ['a.jpg', 'b.jpg', 'c.jpg', 'd.jpg', 'e.jpg', 'f.jpg']
+imagesNames = ['a.jpg', 'b.jpg', 'c.jpg']#, 'd.jpg', 'e.jpg', 'f.jpg']
 scale = (0.3, 0.3)
 images = {} # will have 3 channel color imgs
 imageNos = len(imagesNames)
 m = 3
 k = 4
-
 
 ##########################################################
 #Rescaling
@@ -107,13 +205,11 @@ for img in imagesNames:
     images[img] = temp
 del temp
 
-
 ##########################################################
 #Finding KeyPoints and Discriptors
 ##########################################################
 imageKeyPoints, imageDescriptors = keyPoints(images, imagesNames)
 # retured dictionaries with keys as imageNames
-
 
 ##########################################################
 #Finding matchings for best 'm' matching images for each image
@@ -122,15 +218,25 @@ goodMatchings={}
 for i in range(imageNos-1):
     imgA = imagesNames[i]
     imgB = imagesNames[i+1]
-    goodMatchings[(imgA,imgB)]= keyPointMatching(imageDescriptors, imgA, imgB)
+    goodMatchings[(imgA,imgB)]= keyPointMatching(imageKeyPoints, imageDescriptors, imgA, imgB)
 
-for ke in goodMatchings.keys():
-    findHomoRanSac(goodMatchings, ke)
-
-sys.exit()
 ##########################################################
 #Finding H for each of the pairs of images
 ##########################################################
+n = 500 # iterations
+r = 4 # no of point to calc homo
+t = 4 # pixel threashold
+T = int(0.8*len(goodMatchings)) # majority threashold
+
+# currently for single
+for i in range(1):
+    imgA = imagesNames[i]
+    imgB = imagesNames[i+1]
+    list_kp = goodMatchings[(imgA, imgB)]
+    # print ("length of good keyPoints", len(list_kp[0]))
+    H = findHomoRanSac(n, r, list_kp, t, T)
+    print (H)
+    sys.exit()
 
 
 ##########################################################
@@ -151,87 +257,3 @@ result = stitch(images[2], result, H, s)
 #result = cv.resize(result, (960, 540))
 cv.imshow("correspondences", result)
 cv.waitKey()
-
-
-# How to find the coordinates
-'''
-# Initialize lists
-list_kp1 = []
-list_kp2 = []
-
-# For each match...
-for mat in matches:
-
-    # Get the matching keypoints for each of the images
-    img1_idx = mat.queryIdx
-    img2_idx = mat.trainIdx
-
-    # x - columns
-    # y - rows
-    # Get the coordinates
-    (x1,y1) = kp1[img1_idx].pt
-    (x2,y2) = kp2[img2_idx].pt
-
-    # Append to each list
-    list_kp1.append((x1, y1))
-    list_kp2.append((x2, y2))
-'''
-
-'''
-
-# n is the number of times to repeat ransac
-# t is the number of pixels tolerance allowed
-# T is the ratio of the for which we terminate early.
-def findHomoRanSac(n, m, list_kp1, list_kp2, matches, t, T):
-    for i in range(n):
-        P = make_P(list_kp1, list_kp2)
-        H, Si = findH_Si(P, matches, t, T)
-
-'''
-
-
-'''
-I assume that i recieve 2 lists, in which i have k points
-
-def make_P(list_kp1, list_kp2):
-    k = len(list_kp1)
-
-    # making P matrix
-    P = np.zeros((2k, 9))
-    for i in range(0,2*k,2):
-        pi = np.zeros((2,9))
-        x = list_kp1[i/2][0]
-        x_ = list_kp2[i/2][0]
-        y = list_kp1[i/2][1]
-        y_ = list_kp2[i/2][1]
-
-        P[i+0,0] = -x
-        P[i+0,1] = -y
-        P[i+0,2] = -1
-        P[i+0,6] = x*x_
-        P[i+0,7] = y*x_
-        P[i+0,8] = x_
-        P[i+1,3] = -x
-        P[i+1,4] = -y
-        P[i+1,5] = -1
-        P[i+1,6] = x*y_
-        P[i+1,7] = y*y_
-        P[i+1,8] = y_
-'''
-
-'''
-def findH_Si(P, matches, t, T):
-    # do svd on P get perlimns H
-
-    # multiply all the matches and find if within tol
-
-    # increase counter if within t
-
-    # if counter crosses T
-        # recalculate H for the Si set
-        # return H and count
-
-    # else
-        # recalculate H for the Si set
-        # return H and count corresp to biggest set
-'''
