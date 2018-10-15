b76d3e9ae151a1faa9f118bf04be07ddff5ec8f5
Apoorv <apoorvagni@hotmail.com>
2018-10-15 21:19:00 +0530

    working part1

--- A3/main1.py
+++ A3/main1.py
@@ -58,10 +58,14 @@ def keyPointMatching(imageKeyPoints, imageDescriptors, imgA, imgB):
     img3 = cv.drawMatchesKnn(images[imgA], imageKeyPoints[imgA], images[imgB],
                              imageKeyPoints[imgB], matches, None, **draw_params)
 
-    cv.imshow("correspondences", img3)
+    # cv.imshow("correspondences", img3)
+    # writing to results folder
+    dirr = './result/'
+    file = dirr + "keypoints_"+imgA+'_'+imgB
+    cv.imwrite(file, img3)
     # print('length of all matches ', len(matches))
     # print('length good matches ', len(good))
-    cv.waitKey()
+    # cv.waitKey()
     # print (len(good[0])) # length of good keyPointS
     return good
 
@@ -167,7 +171,7 @@ Makes a canvas for the panorama
 '''
 def createCanvas(img, factor):
     height, width, chnl = img.shape
-    return np.zeros((height*factor, width*factor, chnl), dtype=np.uint8)
+    return np.zeros((height*factor[0], width*factor[1], chnl), dtype=np.uint8)
 
 '''
 brief:
@@ -183,16 +187,19 @@ def drawOnCanvas(canvas, img, H, offset, fill):
     height, width, chnl = img.shape
     for i in range(height):
         for j in range(width):
-            vctr = np.array([[i,j,1]]).T #col vctr
+            vctr = np.array([[j,i,1]]).T #col vctr
             vctr2 = np.matmul(H, vctr)
             vctr2 /= vctr2[2,0] #last coordinate to 1
             pt = vctr2[:2,:] #getting rid of last coordinate
             pt += np.array(offset).T
 
             #drawing with interpolation
-            y = int(pt[0,0])
-            x = int(pt[1,0])
-            canvas[y:y+fill, x:x+fill] = np.full((fill, fill, 3), img[i,j], dtype=np.uint8)
+            x = int(pt[0,0])
+            y = int(pt[1,0])
+            try:
+                canvas[y:y+fill, x:x+fill] = np.full((fill, fill, 3), img[i,j], dtype=np.uint8)
+            except:
+                print('not able to print, x,y', x, y)
             # if i == 0 and j == 0:
             #     print (np.full((fill, fill, 3), img[i,j]))
             #     print (np.full((fill, fill, 3), img[i,j]).shape)
@@ -203,8 +210,8 @@ def drawOnCanvas(canvas, img, H, offset, fill):
 #Reading files
 ##########################################################
 path = './Images_Asgnmt3_1/I1/'
-imagesNames = ['a.jpg', 'b.jpg', 'c.jpg']#, 'd.jpg', 'e.jpg', 'f.jpg']
-scale = (0.1, 0.1)
+imagesNames = ['a.jpg', 'b.jpg', 'c.jpg', 'd.jpg']#, 'e.jpg', 'f.jpg']
+scale = (0.2, 0.2)
 images = {} # will have 3 channel color imgs
 imageNos = len(imagesNames)
 m = 3
@@ -223,64 +230,95 @@ del temp
 ##########################################################
 #Finding KeyPoints and Discriptors
 ##########################################################
+print('finding keypoints and discriptors')
 imageKeyPoints, imageDescriptors = keyPoints(images, imagesNames)
+print('done keypoints and discriptors')
 # retured dictionaries with keys as imageNames
 
 ##########################################################
 #Finding matchings for best 'm' matching images for each image
 ##########################################################
+print('finding keymatches')
 goodMatchings={}
 for i in range(imageNos-1):
     imgA = imagesNames[i]
     imgB = imagesNames[i+1]
     goodMatchings[(imgA,imgB)]= keyPointMatching(imageKeyPoints, imageDescriptors, imgA, imgB)
+print('done keymatches')
 
 ##########################################################
 #Finding H for each of the pairs of images
 ##########################################################
-n = 1000 # iterations
+n = 3000 # iterations
 r = 4 # no of point to calc homo
-t = 4 # pixel threashold
+t = 2 # pixel threashold
 Tratio = 0.95 # majority threashold
 
 # currently for single
 Hs = []
 Ss = []
-for i in range(imageNos-1):
+print('finding homographies')
+for i in range(imageNos -1):
     imgA = imagesNames[i]
     imgB = imagesNames[i+1]
     list_kp = goodMatchings[(imgA, imgB)]
     H, S = findHomoRanSac(n, r, list_kp, t, Tratio)
     Hs.append(H)
     Ss.append(S)
+print('done homographies')
 
 ##########################################################
 #Wrapping the images together using H
 ##########################################################
-factor = imageNos+1
-offset = [[400,400]]
+# first factor multiplie height
+factor = [int(imageNos*1.5), int(imageNos*3)]
+# x,y
+offset = [[3000,1000]]
 canvas = createCanvas(images[imagesNames[0]], factor)
 print('image.shape', images[imagesNames[0]].shape)
 print('canvas.shape', canvas.shape)
 
+print('finding realtive homographies')
 # middle term is identity mat
 Hss = {int(imageNos/2): np.eye(3)}
-
 #forward terms filled
 for i in range(int(imageNos/2), imageNos-1):
-    Hss[i+1] = np.matmul(Hs[i], Hss[i])
+    Hss[i+1] = np.matmul(np.linalg.inv(Hs[i]), Hss[i])
 #backward terms filled
 for i in range(int(imageNos/2)-1, -1, -1):
-    Hss[i] = np.matmul(np.linalg.inv(Hs[i]), Hss[i+1])
+    Hss[i] = np.matmul(Hs[i], Hss[i+1])
+print('done realtive homographies')
 
-# print ('Hss0', Hss[2])
-# print ('Hsinv', Hs[1])
+print ('Hss', Hss)
+print ('Hs', Hs)
 
 # drawing
 for i in range(0, imageNos):
+    print('drawing', imagesNames[i])
     drawOnCanvas(canvas, images[imagesNames[i]], Hss[i], offset, abs(int(imageNos/2)-i)+1)
+    print('drawn', imagesNames[i])
 
 # print (canvas)
-plt.imshow(canvas)
-plt.show()
+# cv.imshow("Stitched Panorama", canvas)
+
+print ('stripping')
+# argwhere will give you the coordinates of every non-zero point
+true_points = np.argwhere(canvas)
+# take the smallest points and use them as the top left of your crop
+top_left = true_points.min(axis=0)
+# take the largest points and use them as the bottom right of your crop
+bottom_right = true_points.max(axis=0)
+out = canvas[top_left[0]:bottom_right[0]+1,  # plus 1 because slice isn't
+          top_left[1]:bottom_right[1]+1]  # inclusive
+print('done stripping')
+
+cv.imwrite("stitchedin.jpg", out)
 sys.exit()
+
+'''
+imageBox(images):
+h = 0
+w
+for img in images:
+    h,w,c = img.shape
+'''
